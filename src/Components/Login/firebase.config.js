4a04caf0af299174973454e4b0972ef66499d087
jm mohiuddin <jmmohiuddin5@gmail.com>
2021-04-18 14:12:01 +0600

    tc

--- src/Components/Login/firebase.config.js
+++ src/Components/Login/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCSGN7LZltypAmdV55Cv_LHsWPjKVw3oZo",
-    authDomain: "service-ff551.firebaseapp.com",
-    projectId: "service-ff551",
-    storageBucket: "service-ff551.appspot.com",
-    messagingSenderId: "139349789675",
-    appId: "1:139349789675:web:26b00e9ce9dbff3085e7ae"
-  };
-  export default firebaseConfig;
+  apiKey: "AIzaSyBtdVkoJI8Nq4nVLDrob7i9bJdov3hXRX8",
+  authDomain: "programing-hero-school.firebaseapp.com",
+  projectId: "programing-hero-school",
+  storageBucket: "programing-hero-school.appspot.com",
+  messagingSenderId: "212808802689",
+  appId: "1:212808802689:web:b4de252db188c171f78880",
+};
+export default firebaseConfig;
